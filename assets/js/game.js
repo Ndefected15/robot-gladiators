3be12489f6206049ed30962c59dca43fb2511c7a
Nick D <Ndefected15@gmail.com>
2021-11-03 13:59:23 -0400

    debugged code

--- assets/js/game.js
+++ assets/js/game.js
@@ -84,6 +84,7 @@ var fight = function (enemyName) {
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-  debugger;
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }

7bcd2456edc43e88aa7bfb3c2ddfd3345c5929a6
Nicholas Defex <ndefected15@gmail.com>
2021-11-06 13:13:11 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,8 +35,11 @@ var fight = function (enemyName) {
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = Math.max(0, enemyHealth - playerAttack);
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
+
     console.log(
       playerName +
         " attacked " +
@@ -53,7 +56,7 @@ var fight = function (enemyName) {
       window.alert(enemyName + " has died!");
 
       // award player money for winning
-      playerMoney = Math.max(0, playerMoney - 10);
+      playerMoney = Math.max(0, playerMoney + 10);
 
       // leave while() loop since enemy is dead
       break;
@@ -61,8 +64,10 @@ var fight = function (enemyName) {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = Math.max(0, playerHealth - enemyAttack);
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
+
     console.log(
       enemyName +
         " attacked " +
@@ -94,7 +99,7 @@ var startGame = function () {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
 
       if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -187,5 +192,13 @@ var shop = function () {
       break;
   }
 };
+
+// function to generate a random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 // start the game when the page loads
 startGame();
